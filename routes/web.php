f72de3bf0a59a7883331243909ff2ad0333a0bda
Ashwani Ashwani <ashwani.ashwani@africa.airtel.com>
2022-06-23 10:35:26 +0530

    paytm payment

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/', function () {
 });
 
 //Paytm Payment
+Route::group(['prefix' => 'payment-mobile'], function () {
+    Route::get('/', 'PaymentController@payment')->name('payment-mobile');
+    Route::get('set-payment-method/{name}', 'PaymentController@set_payment_method')->name('set-payment-method');
+});
+
 Route::post('paytm-payment',[PaytmController::Class, 'paytmPayment'])->name('paytm.payment');
 Route::post('paytm-callback',[PaytmController::Class, 'paytmCallback'])->name('paytm.callback');
 Route::get('paytm-purchase',[PaytmController::Class, 'paytmPurchase'])->name('paytm.purchase');
+Route::get('paytm-status', 'PaytmController@getPaymentStatus')->name('paytm-status');
+Route::get('payment-success', 'PaymentController@success')->name('payment-success');
+Route::get('payment-fail', 'PaymentController@fail')->name('payment-fail');
